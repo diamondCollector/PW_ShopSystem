d3b8024f62fccde1354aad87285d692f4c517a82
IG <igor.widawski@gmail.com>
2020-12-28 18:43:59 +0100

    Adding Item.cs class

---
+++ PW_ShopSystem/Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Item 
+{
+    string _name;
+    string _description;
+    float _weight;
+    float _price;
+    Sprite _image;
+}
